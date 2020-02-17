f6e9a40e25bb470543f8f26ce7b003adc2a66220
tenyears94 <742423357@qq.com>
2020-02-17 16:11:56 +0800

    测试新分支

--- book/src/test/java/cn/javamap/book/BookApplicationTests.java
+++ book/src/test/java/cn/javamap/book/BookApplicationTests.java
@@ -1,7 +1,10 @@
 package cn.javamap.book;
 
+import cn.javamap.book.pojo.User;
+import cn.javamap.book.service.UserService;
 import cn.javamap.book.util.DateUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.support.TestPropertySourceUtils;
 import org.springframework.util.unit.DataUnit;
@@ -12,10 +15,22 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.List;
 
 @SpringBootTest
 class BookApplicationTests {
+    @Autowired
+    private UserService userService;
 
+    @Test
+    public void testUserService(){
+        System.out.println("开始debug");
+        User user1 = new User();
+        List<User> userList = userService.getUserList(user1, null);
+        for (User user : userList) {
+            System.out.println(user);
+        }
+    }
     @Test
     void contextLoads() {
     }
@@ -36,6 +51,11 @@ class BookApplicationTests {
     @Test
     public void testGit(){
         System.out.println("git修改啦");
+    }
+
+    @Test
+    public void testBranch(){
+        System.out.println("测试新分支");
     }
 
 }
